e95d557d65d5ce94633dbf4892e04950d58d92a2
Renata <renatagobetti@live.com>
2024-04-16 19:42:00 +0100

    Exemplo.R

--- Exemplo.R
+++ Exemplo.R
@@ -1,1 +1,16 @@
-#Versao 1
+database <- read.csv(file.choose())
+
+str(database)
+
+database_estudo <- database[, c("Entity", "Year", "Access.to.electricity....of.population.", 
+                                "Access.to.clean.fuels.for.cooking", 
+                                "Renewable.electricity.generating.capacity.per.capita", 
+                                "gdp_per_capita")]
+
+names(database_estudo) <- c("pais", "ano", "acesso_eletricidade_pct", 
+                            "acesso_combustiveis_limpos_pct", 
+                            "capacidade_geracao_electricidade_renovavel_per_capita", 
+                            "pib_per_capita")
+
+head(database_estudo)
+
